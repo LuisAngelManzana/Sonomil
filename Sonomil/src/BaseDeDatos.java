cb45702c58de75a42e3d042dae98a6d8c24136f6
Luis Angel Manzana <manzanaalive@gmail.com>
2018-04-19 21:19:57 -0500

    Actualizacion BD

---
+++ Sonomil/src/BaseDeDatos.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+/**
+ *
+ * @author Manzana
+ */
+public class BaseDeDatos {
+    private Connection conexion;
+    public boolean conectar() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            conexion = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:8889/Sonomil", "root", "root");
+            if (conexion != null) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public void desconectar() {
+        try {
+            this.conexion.close();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
